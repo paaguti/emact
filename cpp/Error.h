261e9119e1193eeda5fa12eb2123937cf30443b3
cjullien <cjullien@3a9aed97-b8d8-4132-81ea-85065a483db5>
2019-11-25 15:41:30 +0000

    Make Error a real class

---
+++ cpp/Error.h
@@ -0,0 +1,39 @@
+/*
+ * static auto rcsid("$Id: ./emacs.h,v 1.66 2018/09/09 07:25:14 jullien Exp $");
+ */
+
+/*
+ * This  program  is  free  software;  you can redistribute it and/or
+ * modify  it  under  the  terms of the GNU General Public License as
+ * published  by  the  Free  Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This  program  is  distributed in the hope that it will be useful,
+ * but  WITHOUT ANY WARRANTY;  without  even the implied  warranty of
+ * MERCHANTABILITY  or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You  should have received a copy of the GNU General Public License
+ * along  with  this  program;  if  not,  write  to the Free Software
+ * Foundation,  Inc.,  59  Temple  Place  -  Suite  330,  Boston,  MA
+ * 02111-1307, USA.
+ */
+
+#if !defined(__ERROR_H)
+#define __ERROR_H
+/**
+ * This class searches for errors in compilation buffer.
+ */
+class Error {
+ public:
+  static void clear();
+  static CMD  next();
+
+ private:
+  static bool get();
+
+  static bool   _flag;
+  static int    _linenum;
+  static EMCHAR _fname[NFILEN];
+};
+#endif /* __ERROR_H */
